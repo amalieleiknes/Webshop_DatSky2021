cc6cf3f61f8291fd2c98130adce41ce7011c9c73
Caroline Jetteberg <s313564@oslomet.no>
2021-04-21 20:38:38 +0200

    Lagt inn metode for å hente ut ordre og produkt med ID

--- src/main/java/portfolio2/packages/Objects/ProductRegister.java
+++ src/main/java/portfolio2/packages/Objects/ProductRegister.java
@@ -1,19 +1,27 @@
 package portfolio2.packages.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 
+@Component
 public class ProductRegister {
+    @Autowired
     private static ArrayList<Product> productRegister = new ArrayList<>();
 
+    @Autowired
     public static ArrayList<Product> getProductRegister() {
         return productRegister;
     }
 
+    @Autowired
     public static void addProduct(Product product){
         productRegister.add(product);
     }
 
     //Removes object from productRegister (not a required function)
+    @Autowired
     public static void deleteProduct(Product product){
         if(product == null){
             return;
@@ -25,6 +33,18 @@ public class ProductRegister {
         }
     }
 
+    @Autowired
+    public static Product getProductByID(int productID){
+        Product ret = null;
+        for(Product p : productRegister){
+            if(p.getProductID() == productID){
+                ret = p;
+            }
+        }
+        return ret;
+    }
+
+    @Autowired
     public static int generateProductID(){
         int productID = 0;
         return productID;
