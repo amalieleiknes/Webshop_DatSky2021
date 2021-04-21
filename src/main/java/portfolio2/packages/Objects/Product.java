629b1e91d528c0090fd8a5a8fac9d74807eda19a
Caroline Jetteberg <s313564@oslomet.no>
2021-04-21 11:09:20 +0200

    Opprettet Product- og ProductRegister-klasser

---
+++ src/main/java/portfolio2/packages/Objects/Product.java
@@ -0,0 +1,67 @@
+package portfolio2.packages.Object;
+
+public class Product {
+    private int productID;
+    private String name;
+    private String shortDescription;
+    private String longDescription;
+    private Double price;
+    private String imageUrl;
+
+    public Product(int productID, String name, String shortDescription, String longDescription, Double price, String imageUrl){
+        this.productID = productID;
+        this.name = name;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/java/portfolio2/packages/Objects/ProductRegister.java
@@ -0,0 +1,27 @@
+package portfolio2.packages.Object;
+
+import java.util.ArrayList;
+
+public class ProductRegister {
+    private static ArrayList<Product> productRegister = new ArrayList<>();
+
+    public static ArrayList<Product> getProductRegister() {
+        return productRegister;
+    }
+
+    public static void addProduct(Product product){
+        productRegister.add(product);
+    }
+
+    //Removes object from productRegister (not a required function)
+    public static void deleteProduct(Product product){
+        if(product == null){
+            return;
+        }
+        for(Product p : productRegister){
+            if(product.equals(p)){
+                productRegister.remove(product);
+            }
+        }
+    }
+}
