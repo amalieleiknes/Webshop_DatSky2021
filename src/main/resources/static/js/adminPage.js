9e02c21a4bd0efd9d98744013be5b087a6aea729
Amalie <amalie.leiknes@hotmail.com>
2021-04-22 17:33:34 +0200

    Jobber med frontend Admin endre produkt

---
+++ src/main/resources/static/js/adminPage.js
@@ -0,0 +1,56 @@
+"use strict";
+$(function(){
+
+    // getting all products
+
+
+
+    // function to format the table
+    function formatProductData(products){
+        let table="<table class='table table-condensed'>" +
+            "<thead><tr><th>Name</th><th>Short Description</th><th>Long Description</th><th>Price</th>" +
+            "<th>image</th><th>Change and Save</th><th>Delete</th><th>Copy</th></thead><tbody>";
+        let line=1;
+        $.each(products, function( key, product) {
+            table+="<tr><td><input type='text' readonly id='productID"+line+"' size='10' value='"+product.productID+"'/></td>" +
+                "<td><input type='text' id='name"+line+"' value='"+product.name+"'/></td>" +
+                "<td><input type='text' id='shortDescription"+line+"' value='"+product.shortDescription+"'/></td>" +
+                "<td><input type='text' id='longDescription"+line+"' value='"+product.longDescription+"'/></td>" +
+                "<td><input type='text' id='price"+line+"' value='"+product.price+"'/></td>"+
+                "<td><input type='text' readonly id='image"+line+"' size=12 value='"+product.image+"'/></td>" +
+                "<td><a class='btn btn-success' onclick='changeProduct("+line+")'>Save</button></td>" +
+                "<td><a class='btn btn-danger' onclick='deleteProduct("+line+")'>Delete</button></td>" +
+                "<td><a class='' onclick='copyProduct("+line+")'>Copy</button></td>" +
+                "</tr>";
+            line++;
+        });
+        table +="</tbody></table>";
+        return table;
+    }
+
+
+
+    // function to save changes to your product
+    function changeProduct(line){
+
+    }
+
+    // delete the selected line
+    function deleteProduct(line){
+
+    }
+
+    // copying a line that will be added with a new productnumber at the end of the line
+    function copyProduct(line){
+
+    }
+
+    // adding a new blank line that can be edited
+    function addProduct(){
+
+    }
+
+
+
+
+});
---
+++ src/main/resources/static/js/productpage.js
@@ -0,0 +1,11 @@
+"use strict";
+$(function(){
+
+   // getting one product
+
+
+
+
+
+
+});
